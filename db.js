05417fde665d58ff34bf3d98e31cc78e8ec34ad0
iveryhatecoding <167974378+iveryhatecoding@users.noreply.github.com>
2025-05-19 16:30:28 +0800

    Update db.js

--- db.js
+++ db.js
@@ -2,5 +2,8 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_5ayUd8ZAVBLG@ep-raspy-union-a15tf0bt-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
+  connectionString: 'postgresql://neondb_owner:npg_5ayUd8ZAVBLG@ep-raspy-union-a15tf0bt-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
